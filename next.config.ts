db24a6bf7df6b1999344284e31e79de6bc96e8fe
Rishal-KV <rishkv923@gmail.com>
2025-08-19 14:57:05 +0530

    changes in the redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
   eslint: {
     // !! This will allow production builds even if there are lint errors
     ignoreDuringBuilds: true,
